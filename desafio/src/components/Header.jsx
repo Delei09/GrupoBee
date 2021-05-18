d1fc5b8326025bff0c5147b3e82527534d7bb608
Delei09 <vanderlei_09@outlook.com>
2021-05-18 09:27:33 -0300

    Construindo componente Header

--- desafio/src/components/Header.jsx
+++ desafio/src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Header.css'
+import LogoBee from '../img/logo-bee-2_semfundo 1.png'
+import LogoBigtester from '../img/421.png'
 
  const props = () => (
      <header className  = 'header' >
-         header
+         <div className="logo">
+             <a href = '#' > <img className = 'logoBee' alt = 'logo Grupo Bee' src = {LogoBee}  /></a>
+         </div>
+         <div className="logo">
+             <a href = '#' > <img className = 'logoBigTester' alt = 'logo bigTester' src = {LogoBigtester}  /></a>
+         </div>
+
      </header>
  )
 
